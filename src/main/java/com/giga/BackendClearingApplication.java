f8fa2e0d78263984239c176eb6ac78b0e2bfea05
kisson666 <704321406@qq.com>
2017-12-31 01:09:08 +0800

    init project

---
+++ src/main/java/com/giga/BackendClearingApplication.java
@@ -0,0 +1,12 @@
+package com.giga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendClearingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendClearingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/giga/BackendClearingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.giga;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendClearingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
